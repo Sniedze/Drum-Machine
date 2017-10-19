070978172a50d3d517ac89996fb6bea09af00b01
Sniedze <u.sniedze@gmail.com>
2017-10-19 16:21:19 +0200

    formed keys

---
+++ drums.js
@@ -0,0 +1,41 @@
+let a=document.querySelector(".a");
+let s=document.querySelector(".s");
+let d=document.querySelector(".d");
+let w=document.querySelector(".w");
+let x=document.querySelector(".x");
+let z=document.querySelector(".z");
+let aa=document.querySelector(".aa");
+let ss=document.querySelector(".ss");
+let dd=document.querySelector(".dd");
+let ww=document.querySelector(".ww");
+let xx=document.querySelector(".xx");
+let zz=document.querySelector(".zz");
+
+window.addEventListener("keydown", function(e){
+    let aa=document.querySelector(".aa");
+    console.log(aa);
+    aa.play();
+    });
+
+/*s.addEventListener("keydown", pressS);
+function pressS(){
+    console.log("S Pressed")
+    ss.play();
+}
+d.addEventListener("keydown", pressD);
+function pressD(){
+    console.log("D Pressed")
+    dd.play();
+}
+w.addEventListener("keydown", pressW);
+function pressW(){
+    ww.play();
+}
+x.addEventListener("keydown", pressX);
+function pressX(){
+    xx.play();
+}
+z.addEventListener("keydown", pressZ);
+function pressZ(){
+    zz.play();
+}*/
