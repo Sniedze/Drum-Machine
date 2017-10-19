e90041f73328a01864ff5a2c6d2a41d5d12a6f15
Sniedze <u.sniedze@gmail.com>
2017-10-19 17:15:44 +0200

    JS

--- drums.js
+++ drums.js
@@ -10,12 +10,45 @@ let dd=document.querySelector(".dd");
 let ww=document.querySelector(".ww");
 let xx=document.querySelector(".xx");
 let zz=document.querySelector(".zz");
+let key=document.querySelector(".key")
 
-window.addEventListener("keydown", function(e){
-    let aa=document.querySelector(".aa");
-    console.log(aa);
-    aa.play();
-    });
+window.addEventListener("keydown", clicked)
+    function clicked(evt){
+        key.classList.add("playing");
+        switch(evt.key){
+
+            case "a":
+                aa.currentTime=0;//rewind to the start
+                aa.play();
+                console.log("A Clicked");
+                break;
+            case "s":
+                ss.currentTime=0;
+                ss.play();
+                console.log("S Clicked");
+                break;
+            case "d":
+                dd.currentTime=0;
+                dd.play();
+                console.log("D Clicked");
+                break;
+            case "w":
+                ww.currentTime=0;
+                ww.play();
+                console.log("W Clicked");
+                break;
+            case "x":
+                xx.currentTime=0;
+                xx.play();
+                console.log("X Clicked");
+                break;
+            case "z":
+                zz.currentTime=0;
+                zz.play();
+                console.log("Z Clicked");
+                break;
+                      }
+    }
 
 /*s.addEventListener("keydown", pressS);
 function pressS(){
